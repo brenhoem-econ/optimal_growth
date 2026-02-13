6a00e6d5fd178ce75ec19f96d67f34b092f46bf9
brenhoem-econ <bhoem2@nebraska.edu>
2026-02-13 15:08:54 -0600

    Update optimal_growth.jl
    My version of the optimal growth model in problem set 1!

--- code/optimal_growth_bad.jl
+++ code/optimal_growth_bad.jl
@@ -1,53 +1,73 @@
-using Parameters, Plots #read in necessary packages
+##Problem 4 
+#construct key parameters and initial grids for the analysis
+@with_kw struct Primitives 
+    β::Float64 = 0.99  #parameters
+    θ::Float64 = 0.36 
+    δ::Float64 = 0.025
+    k_grid::Array{Float64,1} = collect(range(1.0, length = 1000, stop = 45.0))  #capital grid
+    nk::Int64 = length(k_grid)  #number of capital elements to loop over
+    markov::Array{Float64,2} = [0.977 0.023; 0.074 0.926]  #markov transition matrix
+    z_grid::Array{Float64,1} = [1.25, 0.2]  #productivity state grid
+    nz::Int64 = length(z_grid) 
+end 
 
-#global variables instead of structs
-β = 0.99 #discount rate.
-θ = 0.36 #capital share
-δ = 0.025 #capital depreciation
-k_grid = collect(range(1.0, length = 1800, stop = 45.0)) #capital grid
-nk = length(k_grid) #number of capital elements
+#build a MUTABLE struct to hold the model results as it iterates 
+mutable struct Results 
+    val_func::Array{Float64,2} 
+    pol_func::Array{Float64,2} 
+end 
 
-#initialize value function and policy functions, again as globals.
-val_func = zeros(nk)
-pol_func = zeros(nk)
 
-#Bellman operator. Note the lack of type declarations inthe function -- another exaple of sub-optimal coding
-function Bellman(val_func, pol_func)
-    v_next = zeros(nk)
+#function that executes the model and returns results 
+function solve_model()
+    prim = Primitives() #initial primitives brought in for use
+    val_func = zeros(prim.nk, prim.nz)  #preallocate value function as zero vector
+    pol_func = zeros(prim.nk, prim.nz)  #pre allocate policy function as zero vector
+    res = Results(val_func, pol_func)  #initialize the results 
+    v_iterate(prim, res) #value function iteration
+    prim, res  #return deliverables
+end 
 
-    for i_k = 1:nk #loop over state space
-        candidate_max = -1e10 #something crappy
-        k = k_grid[i_k]#convert state indices to state values
-        budget = k^θ + (1-δ)*k #budget given current state. Doesn't this look nice?
 
-        for i_kp = 1:nk #loop over choice of k_prime
-            kp = k_grid[i_kp]
-            c = budget - kp #consumption
-            if c>0 #check to make sure that consumption is positive
-                val = log(c) + β * val_func[i_kp]
-                if val>candidate_max #check for new max value
-                    candidate_max = val
-                    pol_func[i_k] = kp #update policy function
-                end
-            end
-        end
-        v_next[i_k] = candidate_max #update next guess of value function
-    end
-    v_next, pol_func
-end
+#value function iteration program. goal is to AVOID INEFFICIENT GLOBAL GARBAGE 
+function v_iterate(prim::Primitives, res::Results; tol::Float64 = 1e-10) 
+    error = 100 #starting error
+    n = 0  #counter 
+    while error>tol #main convergence loop 
+        n+=1 
+        v_next = bellman(prim, res) #execute bellman function 
+        error = maximum(abs.(v_next - res.val_func)) #reset error term 
+        res.val_func = v_next #update value function in the results vector 
+    end 
+    println("Value functions converges in ", n, " iterations") 
+end 
 
-#more bad globals
-error = 100
-n = 0
-tol = 1e-4
-while error>tol
-    global n, val_func, error, pol_func #declare that we're using the global definitions of these variables in this loop
-    n+=1
-    v_next, pol_func = Bellman(val_func, pol_func)
-    error = maximum(abs.(val_func - v_next)) #reset error term
-    val_func = v_next #update value function held in results vector
-    println(n, "  ",  error)
-end
-println("Value function converged in ", n, " iterations.")
 
-#############
+#bellman operator
+function bellman(prim::Primitives, res::Results) 
+    @unpack β, δ, θ, nz, nk, z_grid, k_grid, markov = prim  #unpack parameters to improve readability 
+    v_next = zeros(nk, nz) 
+
+    for i_k = 1:nk, i_z = 1:nz   #loop over the state space 
+        candidate_max = -1e100   #definitely not going to be the maximum 
+        k, z = k_grid[i_k], z_grid[i_z]
+        budget = z*k^θ + (1 - δ)*k   #budget given values in current state 
+
+        for i_kp = 1:nk  #loop over choice of k_prime 
+            kp = k_grid[i_kp] 
+            c = budget - kp  #consumption 
+            if c>0  #ensure consumption is positive 
+                val = log(c) + β * sum(res.val_func[i_kp,:].*markov[i_z, :])
+                if val>candidate_max #check for new max value 
+                    candidate_max = val 
+                    res.pol_func[i_k, i_z] = kp  #update policy function 
+                end 
+            end 
+        end 
+        v_next[i_k, i_z] = candidate_max  #update next guess of val function 
+    end 
+    v_next 
+end 
+
+prim, res = solve_model() 
+
